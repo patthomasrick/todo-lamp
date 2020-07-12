0d1b749b8455af54f662ed29646e07604e6a1e77
Patrick Thomas <pwt5ca@virginia.edu>
2020-07-11 20:38:30 -0400

    All details

--- src/app/all-tasks-view/all-tasks-view.component.ts
+++ src/app/all-tasks-view/all-tasks-view.component.ts
@@ -3,6 +3,10 @@ import { ApiService } from '../api.service';
 import { Task, Priority } from '../task';
 import { FormBuilder } from '@angular/forms';
 
+enum SORT_BY {
+  date_created, date_due, name
+}
+
 @Component({
   selector: 'app-all-tasks-view',
   templateUrl: './all-tasks-view.component.html',
@@ -15,7 +19,9 @@ export class AllTasksViewComponent implements OnInit {
   sessionID: string;
   selected_task: number;
 
-  all_tasks: Map<number, Task>;
+  all_tasks: Map<number, Task> = new Map();
+  current_tasks: Array<number> = new Array();
+  finished_tasks: Array<number> = new Array();
 
   createTaskForm;
 
@@ -24,13 +30,27 @@ export class AllTasksViewComponent implements OnInit {
     this.apiService.getUsername().subscribe(data => { this.username = data; });
     this.apiService.getSessionID().subscribe(data => { this.sessionID = data; });
     this.apiService.getSelectedTaskID().subscribe(data => { this.selected_task = data; });
-    this.apiService.getTasks().subscribe(data => { setTimeout(() => { this.all_tasks = data; }); });
+    this.apiService.getTasks().subscribe(
+      data => {
+        setTimeout(() => {
+          this.all_tasks = data;
+          this.current_tasks = new Array();
+          this.finished_tasks = new Array();
+          this.all_tasks.forEach((value, key, map) => {
+            if (value.done == 0) {
+              this.current_tasks.push(key);
+            } else {
+              this.finished_tasks.push(key);
+            }
+          });
+          this.sortTasks(SORT_BY.date_created);
+        });
+      });
 
     this.createTaskForm = this.formBuilder.group({ name: "", description: "", priority: "" });
   }
 
   ngOnInit() {
-    this.all_tasks = new Map();
     this.createTaskForm.setValue({ name: "", description: "", priority: "normal" });
   }
 
@@ -41,5 +61,41 @@ export class AllTasksViewComponent implements OnInit {
   createTask(values) {
     this.apiService.createTask(values.name, values.description, values.priority);
     this.createTaskForm.setValue({ name: "", description: "", priority: "normal" });
+  }
+
+  getNotDoneTasks() {
+    var $tasks = new Array<Task>();
+    this.all_tasks.forEach((value, key, map) => {
+      if (value.done == 0) {
+        $tasks.push(value);
+      }
+    });
+    return $tasks;
+  }
+
+  sortTasks(s: SORT_BY) {
+    var $created = (a: number, b: number) => {
+      var $taskA = this.all_tasks.get(a);
+      var $taskB = this.all_tasks.get(b);
+      return $taskA.date_created.valueOf() - $taskB.date_created.valueOf();
+    };
+    var $due = (a: number, b: number) => {
+      var $taskA = this.all_tasks.get(a);
+      var $taskB = this.all_tasks.get(b);
+      return $taskA.date_due.valueOf() - $taskB.date_due.valueOf();
+    };
+    var $name = (a: number, b: number) => {
+      var $taskA = this.all_tasks.get(a);
+      var $taskB = this.all_tasks.get(b);
+      return $taskA.name.localeCompare($taskB.name);
+    };
+    
+    if (s == SORT_BY.date_created) {
+      this.current_tasks = this.current_tasks.sort($created);
+    } else if (s == SORT_BY.date_due) {
+      this.current_tasks = this.current_tasks.sort($due);
+    } else if (s == SORT_BY.name) {
+      this.current_tasks = this.current_tasks.sort($name);
+    }
   }
 }
--- src/app/task-details-view/task-details-view.component.ts
+++ src/app/task-details-view/task-details-view.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Task } from '../task';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details-view',
@@ -12,9 +13,20 @@ export class TaskDetailsViewComponent implements OnInit {
   all_tasks: Map<number, Task>;
   task_index: number = -1;
 
+  detailsForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
+
   constructor(private apiService: ApiService) {
     this.apiService.getTasks().subscribe(data => { this.all_tasks = data; });
-    this.apiService.getSelectedTaskID().subscribe(data => { this.task_index = data; });
+    this.apiService.getSelectedTaskID().subscribe(data => {
+      this.task_index = data;
+      this.detailsForm.setValue({
+        name: this.all_tasks.get(this.task_index).name,
+        description: this.all_tasks.get(this.task_index).description,
+      });
+    });
   }
 
   ngOnInit(): void {
@@ -22,5 +34,13 @@ export class TaskDetailsViewComponent implements OnInit {
 
   currentTask() {
     return this.all_tasks.get(this.task_index);
+  }
+
+  saveDetails() {
+    
+  }
+
+  deleteActiveTask() {
+
   }
 }
