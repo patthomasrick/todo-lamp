b2727a3ccc26780a682e20a8c6104adcd761ce66
Patrick Thomas <pwt5ca@virginia.edu>
2021-04-02 17:21:03 -0400

    Refresh project and fix dependencies

--- upload.sh
+++ upload.sh
@@ -2,6 +2,6 @@
 
 ng build --prod --optimization
 tar -cvf todo.tar dist/todo-patrickwthomas-net
-scp todo.tar pwt5ca@patrickwthomas.net:~/
-ssh pwt5ca@patrickwthomas.net 'rm -rf dist dev.patrickwthomas.net && tar -xvf todo.tar && cp dist/todo-patrickwthomas-net dev.patrickwthomas.net -r'
+scp todo.tar hostgator:~/
+ssh hostgator 'rm -rf dist dev.patrickwthomas.net && tar -xvf todo.tar && cp dist/todo-patrickwthomas-net dev.patrickwthomas.net -r'
 
